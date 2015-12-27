048b4001e5bdf625ec223c3c70e80e4f67e65101
gskema <gytis@reservationpartner.lt>
2015-12-27 14:45:04 +0200

    [*] Change .css output style to :expanded

--- themes/community-theme-16/config.rb
+++ themes/community-theme-16/config.rb
@@ -8,7 +8,7 @@ images_dir = "img"
 javascripts_dir = "js"
 fonts_dir = "fonts"
 
-output_style = :nested
+output_style = :expanded
 environment = :development
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
