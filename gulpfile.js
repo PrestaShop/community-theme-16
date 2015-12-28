4f39221d53a35284dcf36676e3a90426ae802517
gskema <gytis@reservationpartner.lt>
2015-12-28 14:47:09 +0200

    [-] Run build tasks in a sequence.

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var exec   = require('child_process').exec;
 var argv   = require('yargs').argv;
 var fs     = require('fs-extra');
 var zip    = require('gulp-zip');
+var runSequence = require('run-sequence');
 
 var createFolders = [
     './themes/community-theme-16/cache/',
@@ -21,39 +22,72 @@ var removeTrash = [
     './themes/community-theme-16/css/**/*.css.map'
 ];
 
-gulp.task('create-folders', function(){
-    createFolders.map(function(path){
+gulp.task('create-folders', function(callback){
+    var total = createFolders.length;
+    var done  = 0;
+
+    if (total < 1 && callback) {
+        callback();
+    }
+
+    createFolders.forEach(function(path){
         mkdirp(path, function (err) {
             if (err) {
                 console.error(err);
             } else {
                 console.log('Created folder : ' + path);
             }
+
+            done++;
+            if (done == total && callback) {
+                callback();
+            }
         });
     });
 });
 
-gulp.task('build-css', function(){
+gulp.task('build-css', function(callback){
     var options = '';
     if (argv.f || argv.force) {
         options += ' --force';
     }
-    var compassCompile = exec('compass compile ./themes/community-theme-16'+ options);
+    var compassCompile = exec('compass compile ./themes/community-theme-16'+ options, function(err, out, code) {
+        if (err instanceof Error) {
+            throw err;
+        }
+
+        if (callback) {
+            callback();
+        }
+    });
     compassCompile.stdout.pipe(process.stdout);
 });
 
 gulp.task('remove-trash', function(){
-    del(removeTrash).then(function() {
+    return del(removeTrash).then(function() {
         console.log('Deleted files and folders:\n', removeTrash.join('\n'));
     });
 });
 
-gulp.task('copy-index', function(){
+gulp.task('copy-index', function(callback){
+    var total;
+    var done  = 0;
     glob('themes/community-theme-16/**/', { ignore : copyIndexIgnore }, function(err, folders) {
+        total = folders.length;
+        if (total < 1 && callback) {
+            callback();
+        }
+
+        // console.log('Copy to folders: \n', folders.join('\n'));
         folders.forEach(function(folder) {
             fs.copy('index.php', folder + '/index.php', function(err) {
                 if (err) {
                     return console.error(err);
+                }
+
+                done++;
+                if (done == total && callback) {
+                    callback();
                 }
             });
         });
@@ -74,12 +108,20 @@ gulp.task('create-zip', function(){
             themeVersion = matches[1].trim();
         }
 
-        gulp.src(['./themes/**', './Config.xml'])
+        return gulp.src(['./themes/**', './Config.xml'])
             .pipe(zip('v' + themeVersion + '-community-theme-16.zip'))
             .pipe(gulp.dest('./'));
     });
 });
 
-gulp.task('build', ['create-folders', 'build-css', 'remove-trash', 'copy-index', 'create-zip']);
+gulp.task('build', function(callback) {
+    runSequence(
+        ['create-folders', 'build-css'],
+        'remove-trash',
+        'copy-index',
+        'create-zip',
+        callback
+    );
+});
 
 gulp.task('default', ['build']);
