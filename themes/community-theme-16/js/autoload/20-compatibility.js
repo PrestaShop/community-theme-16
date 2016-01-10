5839b90fd45d2f85eaaa59376eb33963b3036cfd
gskema <gytis@reservationpartner.lt>
2016-01-10 20:24:53 +0200

    [+] JS: Add .js file to put compatibility code

---
+++ themes/community-theme-16/js/autoload/20-compatibility.js
@@ -0,0 +1,8 @@
+/**
+ * This file contains compatibility code
+ * for code and functions that were removed from this theme.
+ *
+ * If you are developing a custom shop with this theme,
+ * you may remove this file if you can take care of any
+ * theme compatibility issues with custom modules.
+ */
