a30389fa1d31e07fb4b31d136104a258a4b8d7db
CedCannes <CedCannes@users.noreply.github.com>
2016-11-09 22:57:21 +0100

    Update blocklayered.js

--- themes/community-theme-16/js/modules/blocklayered/blocklayered.js
+++ themes/community-theme-16/js/modules/blocklayered/blocklayered.js
@@ -8,7 +8,7 @@ $(function() {
   cancelFilter();
 
   // Click on color
-  $(document).on('click', '#layered_form input[type=button], #layered_form label.layered_color', function() {
+  $(document).on('click', '#layered_form input[type=button], #layered_form label.layered_color a', function(e) {
     e.preventDefault();
     var $hiddenInput = $('input[name=' + $(this).attr('name') + '][type=hidden]');
     if (!$hiddenInput.length) {
